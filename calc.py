8023191800f5403fd7f0912857be08f94eac7a34
Kevinrocks100 <Liukevin209@gmail.com>
2021-07-08 14:29:11 +0000

    description of changes

---
+++ calc.py
@@ -0,0 +1,17 @@
+# Greet the user.
+
+print ("Hello. I'm a fortune teller")
+
+# Ask the user if they want their function told.
+
+answer = input("Do you want a fortune? ")
+while answer.lower() != "no":
+    print("User asked for a fortune")
+    answer = input("Do you want a fortune? ")
+
+# Fortune given.
+
+# Ask if user wants another fortune.
+
+# Repeat until user quits.
+
